04e5d16a7468073eeeaf504c5753aeb20a3175e9
berfinbudak <adabbudak@gmail.com>
2024-12-08 20:48:35 +0300

    city class
    My first commit

---
+++ City.java
@@ -0,0 +1,25 @@
+public class City {
+    private String label;
+    private int index;
+
+    City(String label, int index) {
+        this.label = label;
+        this.index = index;
+    }
+
+    public void setName(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+}
